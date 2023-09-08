c5eff8b8ef2488fdae9ff675cddaab66de708b92
gahyeon <pgh2031@inpix.com>
2023-09-08 17:55:54 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,12 +1,12 @@
 
 
 enum Shoes {
-  Nike,
-  Adidas,
-  NewBalance
+  Nike = '나이키',
+  Adidas = '아디다스',
+  NewBalance = '뉴발란스'
 }
 
 var myShoes = Shoes.Nike;
-console.log(myShoes); // 0
+console.log(myShoes); // '나이키'
 
 // ★ enum을 문자나 다른 숫자로 초기화하지 않으면, 기본적으로 0부터 시작한 숫자 인덱스가 들어간다.
