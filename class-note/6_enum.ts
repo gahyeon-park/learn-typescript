103f27aefdd88295a8fe20d8423a51c638e2c0d3
gahyeon <pgh2031@inpix.com>
2023-09-08 18:06:19 +0900

    이넘 활용예제

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -10,3 +10,24 @@ var myShoes = Shoes.Nike;
 console.log(myShoes); // '나이키'
 
 // ★ enum을 문자나 다른 숫자로 초기화하지 않으면, 기본적으로 0부터 시작한 숫자 인덱스가 들어간다.
+
+
+// ※ enum 활용 예제
+enum Answer {
+  Yes = 'Y',
+  No = 'N',
+}
+
+function askQuestion(answer: Answer) {
+  if(answer === Answer.Yes) {
+    console.log('정답입니다.');
+  }
+  if(answer === Answer.No) {
+    console.log('오답입니다.');
+  }
+}
+
+askQuestion('예스');
+askQuestion('Y');
+askQuestion('Yes');
+askQuestion(Answer.Yes);
