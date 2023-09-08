21f964656cbb3a21b3f8dced9666e0cec7caa1a3
gahyeon <pgh2031@inpix.com>
2023-09-08 14:16:54 +0900

    first commit

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,10 @@
+// JS 선언
+var strA = "hello";
+
+// TS 선언
+var strB: string = "hello";
+let num: number = 10;
+let arr: Array<number> = [1,2,3];
+let colors: Array<string> = ["red", "green", "yellow"];
+let itemsA: number[] = [1,2,3];
+let itemsB: boolean[] = [true, false];
